71c7805a317763baf131591b45be5a3ac2f2251e
Alex Miller <codex.nz@gmail.com>
2020-10-07 23:57:04 +1300

    test(demo) Update demo to cover public API.

--- demo/app/main/main-page.ts
+++ demo/app/main/main-page.ts
@@ -3,8 +3,8 @@ import * as pages from "tns-core-modules/ui/page";
 import { MainModel } from "./main-view-model";
 
 // Event handler for Page 'loaded' event attached in main-page.xml
-export function pageLoaded(args: observable.EventData) {
+export function pageLoaded(args: observable.EventData): void {
     // Get the event sender
-    const page = <pages.Page>args.object;
+    const page = args.object as pages.Page;
     page.bindingContext = new MainModel();
 }
